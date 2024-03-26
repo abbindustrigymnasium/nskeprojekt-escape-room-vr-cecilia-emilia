5906da10f645569c0191d45eafa923d67fe74c61
abbemirom <92145395+abbemirom@users.noreply.github.com>
2024-03-26 10:24:02 +0100

    Canvaslista, next, back, play

--- Escape Room VR/Assets/Scripts/Menu.cs
+++ Escape Room VR/Assets/Scripts/Menu.cs
@@ -6,8 +6,41 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public List<GameObject> canvasList;
+    public int canvasCount;
+    public void Start()
+    {
+           canvasCount = 0;
+          if (canvasList.Count > 0) { 
+            canvasList[canvasCount].SetActive(true);
+        }
+    }
+
+    public void OnNextClick()
+    {
+        if (canvasCount < canvasList.Count-1) {
+            canvasList[canvasCount].SetActive(false);
+            canvasCount++;
+            canvasList[canvasCount].SetActive(true);
+        }
+        else
+        {
+            OnPlayClick();
+        }
+        Debug.Log(canvasCount);
+    }
+    public void OnBackClick()
+    {
+        if (canvasCount > 0){ 
+            canvasList[canvasCount].SetActive(false);
+            canvasCount--;
+            canvasList[canvasCount].SetActive(true);
+        }
+
+        Debug.Log(canvasCount);
+    }
+
     public void OnPlayClick() {
-        Thread.Sleep(2000);
         SceneManager.LoadScene(1);
         Debug.Log("Funktionen körs!");
     }
